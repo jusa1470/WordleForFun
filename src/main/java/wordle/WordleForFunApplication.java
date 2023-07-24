c84325892a25014c21eb72db63e73d8068abd38c
Julia <sanfordjj1997@gmail.com>
2023-07-23 21:25:42 -0600

    Initial setup

---
+++ src/main/java/wordle/WordleForFunApplication.java
@@ -0,0 +1,13 @@
+package wordle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordleForFunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordleForFunApplication.class, args);
+	}
+
+}
